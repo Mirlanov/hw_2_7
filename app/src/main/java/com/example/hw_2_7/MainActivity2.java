b9760580123914c941ae989045835d9d1b114472
Mirlanov <132361233+Mirlanov@users.noreply.github.com>
2023-07-04 15:13:55 +0600

    Update MainActivity2.java

--- app/src/main/java/com/example/hw_2_7/MainActivity2.java
+++ app/src/main/java/com/example/hw_2_7/MainActivity2.java
@@ -1,6 +1,28 @@
 package com.example.hw_2_7;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity2 extends Activity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.policies);
+        textView.setText(getIntent().getStringExtra("policies"));
+    }
 }
